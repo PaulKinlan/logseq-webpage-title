48f83c84241a3a87e88bf408f3c2a7526ff92dec
Paul Kinlan <paul.kinlan@gmail.com>
2022-05-02 21:20:35 +0100

    Adding Release script + Logo + change licence file name

---
+++ release.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    "@semantic-release/git",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd:
+          "zip -qq -r logseq-webpage-title-${nextRelease.version}.zip dist logo.png README.md LICENSE package.json",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: "logseq-plugin-heatmap-*.zip",
+      },
+    ],
+  ],
+};
