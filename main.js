a31bc6c2f0d5e20a6e2ccc9046913a0bc255fc30
Paul Kinlan <paul.kinlan@gmail.com>
2022-05-10 04:16:23 -0700

    feat(context menu): Right click on block and now you can convert all links

--- main.js
+++ main.js
@@ -14,10 +14,53 @@
 import "@logseq/libs";
 import urlRegex from "url-regex";
 
+async function getTitle(url) {
+  let title = "";
+
+  const response = await fetch(url);
+  const responseText = await response.text();
+  const matches = responseText.match(/<title>([^<]*)<\/title>/);
+  if (matches !== null && matches.length > 1) {
+    title = matches[1].trim();
+  }
+  return title;
+}
+
 function main() {
-  logseq.Editor.registerSlashCommand("Title", async () => {
+  logseq.Editor.registerBlockContextMenuItem(
+    "Get Link Titles",
+    async ({ uuid,blockId }) => {
+      const {content} = await logseq.Editor.getBlock(uuid);
+      let text = content;
+
+      // Get's all the urls.
+      const urls = text.match(urlRegex());
+      let offset = 0;
+
+      for (const url of urls) {
+        const urlIndex = text.indexOf(url, offset);
+        if (
+          text.slice(urlIndex - 2, urlIndex) != "](" ||
+          text.slice(urlIndex + url.length, urlIndex + url.length + 1) != ")"
+        ) {
+          // It's a URL that's not wrapped
+          const title = await getTitle(url);
+          if (title != "") {
+            const start = text.slice(0, urlIndex);
+            const linkifiedUrl = `[${title}](${url})`;
+            const end = text.slice(urlIndex + url.length);
+            text = `${start}${linkifiedUrl}${end}`;
+            offset = urlIndex + url.length;
+          }
+        }
+      }
+
+      await logseq.Editor.updateBlock(uuid, text);
+    }
+  );
+
+  logseq.Editor.registerSlashCommand("Title", async ({ uuid }) => {
     const text = await logseq.Editor.getEditingBlockContent();
-    const { uuid } = await logseq.Editor.getCurrentBlock();
     const { pos } = await logseq.Editor.getEditingCursorPosition();
 
     // Find the first URL after the command.
@@ -26,15 +69,11 @@ function main() {
 
     if (urls.length >= 1) {
       // Just get the first URL
-      const url = urls.keys().next().value;
-      const response = await fetch(url);
-      const responseText = await response.text();
-      const matches = responseText.match(/<title>([^<]*)<\/title>/);
-      if (matches !== null && matches.length > 1) {
-        let title = matches[1];
-        title = title.trim();
+      const url = urls[0];
+      const title = await getTitle(url);
+      if (title != "") {
         const newBlockText = before + after.replace(url, `[${title}](${url})`);
-        logseq.Editor.updateBlock(uuid, newBlockText);
+        await logseq.Editor.updateBlock(uuid, newBlockText);
       }
     }
   });
